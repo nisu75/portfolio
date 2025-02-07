9d8b7a236d6957fd48fab624b1dd38b5427282cf
Nicole Cui <nicolexcui@gmail.com>
2025-02-06 19:43:23 -0500

    add footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,38 @@
+import { arialUnicode } from "../../styles/fonts";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
 export default function Footer() {
     return (
-        <div>
-            <p>
-                
+        <div className="flex flex-col gap-3">
+            <p className={`${arialUnicode.className}`}>
+                ───── ⟢ ・⸝⸝
             </p>
+            <div className="flex flex-row gap-3">
+                <a
+                    href="mailto:nicolexcui@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon icon={faEnvelope} size="lg" className="fa-icon" />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/nicolexcui/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon icon={faLinkedinIn} size="lg" className="fa-icon" />
+                </a>
+                <a
+                    href="https://github.com/nisu75"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon icon={faGithub} size="lg" className="fa-icon" />
+                </a>
+            </div>
         </div>
     );
 }
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from "next";
+import { Viewport } from "next";
 import "../styles/globals.css";
 import { sometypeMono } from "../styles/fonts";
 import NavBar from "./components/navbar";
 import HomeButton from "./components/homebutton";
 import Footer from "./components/footer";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+export const viewport: Viewport = {
+  themeColor: "#FEF0F3",
+}
 
 export const metadata: Metadata = {
   title: {
@@ -29,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <div className="flex-grow mt-1 mx-2 pb-8 md:w-3/5 body">
               {children}
               <Analytics />
-              <div className="mt-8">
+              <div className="mt-4">
                 <Footer />
               </div>
             </div>
